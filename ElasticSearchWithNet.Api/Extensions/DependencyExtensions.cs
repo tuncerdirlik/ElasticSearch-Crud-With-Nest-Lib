44035a8a30d196cbffa0c7089bee0b66cf6bbb80
tuncer.dirlik <tuncerdirlik@hotmail.com>
2023-08-05 12:53:25 +0300

    Add project files.

---
+++ ElasticSearchWithNet.Api/Extensions/DependencyExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Elasticsearch.Net;
+using Nest;
+
+namespace ElasticSearchWithNet.Api.Extensions
+{
+    public static class DependencyExtensions
+    {
+        public static void AddElastic(this IServiceCollection services, IConfiguration configuration)
+        {
+            var pool = new SingleNodeConnectionPool(new Uri(configuration["Elastic:Url"]!));
+            var settings = new ConnectionSettings(pool);
+            var client = new ElasticClient(settings);
+
+            services.AddSingleton(client);
+        }
+    }
+}
---
+++ ElasticSearchWithNet.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,67 @@
+﻿using ElasticSearchWithNet.Api.DTOs;
+using ElasticSearchWithNet.Api.Models;
+using Nest;
+
+namespace ElasticSearchWithNet.Api.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly ElasticClient _client;
+        private const string productIndexName = "products";
+
+        public ProductRepository(ElasticClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<Product?> SaveAsync(Product product)
+        {
+            product.CreatedAt = DateTime.UtcNow;
+
+            var response = await _client.IndexAsync(product, k => k.Index(productIndexName));
+            if (!response.IsValid)
+            {
+                return null;
+            }
+
+            product.Id = response.Id;
+
+            return product;
+        }
+
+        public async Task<List<Product>> GetAllAsync()
+        {
+            var result = await _client.SearchAsync<Product>(k => k.Index(productIndexName).Query(x => x.MatchAll()));
+            foreach (var hit in result.Hits)
+            {
+                hit.Source.Id = hit.Id;
+            }
+
+            return result.Documents.ToList();
+        }
+
+        public async Task<Product?> GetByIdAsync(string id)
+        {
+            var result = await _client.GetAsync<Product>(id, k => k.Index(productIndexName));
+            if (!result.IsValid)
+            {
+                return null;
+            }
+
+            result.Source.Id = result.Id;   
+            return result.Source;
+        }
+
+        public async Task<bool> UpdateAsync(ProductUpdateDto productToUpdate)
+        {
+            var result = await _client.UpdateAsync<Product, ProductUpdateDto>(productToUpdate.Id, k => k.Index(productIndexName).Doc(productToUpdate));
+            return result.IsValid;
+        }
+
+        public async Task<DeleteResponse> DeleteAsync(string id)
+        {
+            var result = await _client.DeleteAsync<Product>(id, k => k.Index(productIndexName));
+            return result;
+        }
+    }
+}
